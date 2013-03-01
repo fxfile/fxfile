7b8dce6a7602d82ae68f9e99a44659cb299e2ab8
flychk@gmail.com <flychk@gmail.com@0416172e-dc33-0410-a0a3-05fd41226e6e>
2013-03-01 05:54:21 +0000

    [A] XPR_PATH_SEPARATOR, XPR_FILE_SEPARATOR

--- trunk/src/xpr/include/xpr_file_sys.h
+++ trunk/src/xpr/include/xpr_file_sys.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012 Leon Lee author. All rights reserved.
+// Copyright (c) 2012-2013 Leon Lee author. All rights reserved.
 //
 // Use of this source code is governed by a GPLv3 license that can be
 // found in the LICENSE file.
@@ -15,16 +15,26 @@ namespace xpr
 #if defined(XPR_CFG_OS_WINDOWS)
 #define XPR_MAX_PATH     MAX_PATH
 #define XPR_MAX_FILENAME _MAX_FNAME
-#define XPR_MAX_PARAM    INFOTIPSIZE
+#define XPR_MAX_ARG      INFOTIPSIZE
 #elif defined(XPR_CFG_OS_LINUX)
 #define XPR_MAX_PATH     PATH_MAX
 #define XPR_MAX_FILENAME FILENAME_MAX
-#define XPR_MAX_PARAM    ARG_MAX
+#define XPR_MAX_ARG      ARG_MAX
 #elif defined(XPR_CFG_OS_POSIX)
 #define XPR_MAX_PATH     _POSIX_PATH_MAX
 #define XPR_MAX_FILENAME _POSIX_FILENAME_MAX
-#define XPR_MAX_PARAM    ARG_MAX
+#define XPR_MAX_ARG      ARG_MAX
 #else // not XPR_CFG_OS_WINDOWS, not XPR_CFG_OS_LINUX, not XPR_CFG_OS_POSIX
+#error Please, support for your OS. <xpr_file_sys.h>
+#endif
+
+#if defined(XPR_CFG_OS_WINDOWS)
+#define XPR_FILE_SEPARATOR '\\'
+#define XPR_PATH_SEPARATOR ';'
+#elif defined(XPR_CFG_OS_POSIX)
+#define XPR_FILE_SEPARATOR '/'
+#define XPR_PATH_SEPARATOR ':'
+#else // not XPR_CFG_OS_WINDOWS, not XPR_CFG_OS_POSIX
 #error Please, support for your OS. <xpr_file_sys.h>
 #endif
 } // namespace xpr
